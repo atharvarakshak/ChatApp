d5319ca86abceb22b6c2f200081b2a6f96d86029
atharvarakshak <atharvarakshak23@gmail.com>
2024-07-12 02:28:51 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ mongoose.connect(process.env.MONGO_URL) .then(()=>{
 });
 
 
+app.get("/",(req,res)=>{
+    res.send('Hello World');
+});
+
+
 const server = app.listen(process.env.PORT ,()=>{
     console.log(`Server is running on port: ${process.env.PORT}`);
 })
