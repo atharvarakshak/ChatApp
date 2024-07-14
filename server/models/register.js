5aeb784bb80b889a3a548de61f62bd461816ac5a
atharvarakshak <atharvarakshak23@gmail.com>
2024-07-14 18:05:58 +0530

    register

--- server/models/register.js
+++ server/models/register.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose')
 
 const RegisterSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+    name: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    password: {
+        type:String,
+        required:true,
+        
+    },
+    isAvatarImageSet:{
+        type:Boolean,
+        default:false,
+    },
+    avatarImage:{
+        type:String,
+        default:""
+    }
+
 })
 
 const RegisterModel = mongoose.model("Users", RegisterSchema);
